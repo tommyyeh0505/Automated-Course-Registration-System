cb61973655c38a7d29fe358bb519a3b4bac513db
Andy Tang <andytang43@gmail.com>
2019-04-26 10:58:14 -0700

    generated upload component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,

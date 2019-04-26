1f719598f3980bc97a85bbd07910c2ce02085cde
Andy Tang <andytang43@gmail.com>
2019-04-26 11:09:00 -0700

    implement upload component

--- Client/src/app/upload/upload.component.ts
+++ Client/src/app/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http'
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  public progress: number;
+  public message: string;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  upload(files) {
+    if (files.length === 0) return;
+
+    const formData = new FormData();
+
+    for (let file of files) {
+      formData.append(file.name, file);
+    }
+
+    const uploadRequest = new HttpRequest('POST', 'url', formData, {
+      reportProgress: true
+    });
+
+    this.http.request(uploadRequest).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress = Math.round(100 * event.loaded / event.total);
+      } else if (event.type === HttpEventType.Response) {
+        this.message = event.body.toString();
+      }
+    });
   }
 
 }

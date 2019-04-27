21075e3625ea7b65767419f956cb70d6841c2605
Andy Tang <32403404+aght@users.noreply.github.com>
2019-04-26 22:30:03 -0700

    file upload and csv reading (#3)
    * created backend project
    
    * added gitkeep for Client folder
    
    * added .gitignore + removed untracked
    
    * added angular front-end + uploadcontroller
    
    * generated upload component
    
    * get changes from master (#1)
    
    * created backend project
    
    * added gitkeep for Client folder
    
    * added .gitignore + removed untracked
    
    * added angular front-end + uploadcontroller
    
    * implement upload component
    
    * added file upload function (in progress)
    
    * renamed folders, finished upload functionality
    
    * added Csv parsing
    
    * Added Traces for debugging +  moved csv data to dynamic list
    
    * added api endpoint to environments.ts

---
+++ API/ACRS/Controllers/UploadController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using CsvHelper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ACRS.Controllers
+{
+    [EnableCors("CORSPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        public ActionResult Upload()
+        {
+            var files = Request.Form.Files;
+
+            if (files.Count == 0)
+            {
+                return BadRequest("No files");
+            }
+
+            foreach (var file in files)
+            {
+                if (file.Length > 0 && Path.GetExtension(file.FileName).Equals(".csv"))
+                {
+                    Trace.WriteLine($"Parsing: {file.Name}");
+                    List<dynamic> data = ParseCsv(file);
+                } else
+                {
+                    Trace.WriteLine($"File has 0 bytes or is not a .csv file - Skipping: {file.Name}");
+                }
+            }
+
+            return Ok();
+        }
+
+        private List<dynamic> ParseCsv(IFormFile file)
+        {
+            List<dynamic> data = new List<dynamic>();
+
+            using (var stream = file.OpenReadStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var csv = new CsvReader(reader);
+                    while (csv.Read())
+                    {
+                        var crn = csv[1];
+                        var courseNo = csv[4];
+                        var name = csv[14];
+                        var id = csv[15];
+                        var grade = csv[33];
+
+                        Trace.WriteLine($"CRN: {crn} Course No: {courseNo} Name: {name} ID: {id} Grade: {grade}");
+
+                        data.Add(new
+                        {
+                            Crn = crn,
+                            CourseNo = courseNo,
+                            FullName = name,
+                            StudentId = id,
+                            Grade = grade
+                        });
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from  '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

49d9f9b6340791117f4ee975996ab9e922e45e4a
tommyyeh0505 <tommyyeh0505@hotmail.com>
2019-04-29 10:55:01 -0700

    added dbcontext and courseapi controller

---
+++ API/ACRS/Controllers/CourseAPIController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ACRS.Models;
+using ACRS.Data;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ACRS.Controllers
+{
+    [Route("api/[controller]")]
+    public class CourseAPIController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CourseAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Course>>> Get()
+        {
+            return await _context.Courses
+                .Include(b => b.Prerequisites)
+                .ToListAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/ACRS/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ACRS.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACRS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}

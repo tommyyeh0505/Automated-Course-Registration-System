e42130ba5bcf8bdf67da941096307c115fb5d93a
Eva <pau7@my.bcit.ca>
2019-04-29 10:57:38 -0700

    add student, user, grade model class

---
+++ API/ACRS/Models/Grade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ACRS.Models
+{
+    public class Grade
+
+    {   [Key]
+        [Column(Order = 0)]
+        [Required]
+        public String stdID { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [Required]
+        public String CRNs { get; set; }
+
+        [Required]
+        public String courseID { get; set; }
+
+        [Required]
+        public String term { get; set; }
+
+        [Required]
+        public DateTime date { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public double finalGrade { get; set; }
+
+    }
+}

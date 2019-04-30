3ad7e1bfe428d27a9ff8ac4508a88eb730dcb49e
Eva <pau7@my.bcit.ca>
2019-04-30 10:46:44 -0700

    rename model class varaible in C# systnax

--- API/ACRS/Models/User.cs
+++ API/ACRS/Models/User.cs
@@ -5,7 +5,11 @@ namespace ACRS.Models
 {
     public class User
 
-    {   [Required]
+    // String start with lowercase 
+    //other public variable start with Capital 
+
+    {
+        [Required]
         [Key]
         [StringLength(20, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 4)]
         public String username { get; set; }

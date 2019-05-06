b4ac4eb2674fd76007e77721d93bd70563fc8bc6
Andy Tang <andytang43@gmail.com>
2019-05-06 14:23:49 -0700

    added way to confirm upload

--- ClientApp/src/app/components/upload/upload.component.ts
+++ ClientApp/src/app/components/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpEvent, HttpEventType } from '@angular/common/http';
+import { UploadService } from 'src/app/services/upload.service';
 
 @Component({
   selector: 'upload',
@@ -8,12 +9,35 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class UploadComponent implements OnInit {
   public progress: number = 0;
+  public isUploading: boolean = false;
+  public files: File[] = [];
 
-  constructor(private http: HttpClientModule) { }
+  constructor(private uploadService: UploadService) { }
 
   ngOnInit() { }
 
-  onProgress(event) {
-    this.progress = event;
+  onFileDrop(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      let file: File = files.item(i);
+
+      this.files.push(file);
+    }
+  }
+
+  upload() {    
+    this.isUploading = true;
+
+    this.uploadService.upload(this.files)
+      .subscribe((event: HttpEvent<any>) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.progress = Math.round(100 * event.loaded / event.total);
+        } else if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+          this.isUploading = false;
+        }
+      }, error => {
+        console.log(error);
+        this.isUploading = false;
+      });
   }
 }

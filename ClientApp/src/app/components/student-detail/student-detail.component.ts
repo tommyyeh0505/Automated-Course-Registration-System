b61769e3882c07c5882240ab7f468af72a87b7c7
mikah13 <anhminhhoang13@gmail.com>
2019-05-09 11:23:50 -0700

    making grade table in student detail

--- ClientApp/src/app/components/student-detail/student-detail.component.ts
+++ ClientApp/src/app/components/student-detail/student-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { StudentService } from 'src/app/services/student.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Student } from 'src/app/models/student';
 import { Grade } from 'src/app/models/grade';
+import { MatTableDataSource, MatPaginator, MatSort, MatDialog } from '@angular/material';
+
 
 @Component({
   selector: 'app-student-detail',
@@ -11,9 +13,16 @@ import { Grade } from 'src/app/models/grade';
 })
 export class StudentDetailComponent implements OnInit {
   student: Student = new Student();
-  // studentGrades: Grade[] = new Grade();
+  studentGrades: Grade[];
+  newGrade: Grade;
+  editGrade: Grade;
+  displayedColumns: string[] = ['courseId', 'crn', 'term', 'finalGrade', 'attempts', 'view', 'edit', 'delete'];
+  dataSource: MatTableDataSource<Grade>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private route: ActivatedRoute,
     private router: Router,
+    public dialog: MatDialog,
     private studentService: StudentService) { }
 
   ngOnInit() {
@@ -22,12 +31,28 @@ export class StudentDetailComponent implements OnInit {
       this.getStudentById(id);
     })
   }
+  initTable(data) {
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.initNewGrade();
+  }
+
 
   getStudentById(studentId: string) {
     this.studentService.getStudent(studentId).subscribe((data: Student) => {
       this.student = data;
+      this.getStudentsGrades(studentId);
     }, err => {
       this.router.navigate(['/error']);
+    })
+  }
+
+  getStudentsGrades(studentId: string) {
+    this.studentService.getStudentGrades(studentId).subscribe((data: Grade[]) => {
+      console.log(data);
+
+      this.studentGrades = data;
     })
   }
 

7f0f03c41bef448c2b03b6796101e4f741089cf2
tommyyeh0505 <tommyyeh0505@hotmail.com>
2019-05-09 12:02:12 -0700

    added dummy functions for course eligability

---
+++ API/ACRS/Models/StudentEligability.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACRS.Models
+{
+    public class StudentEligability
+    {
+
+        public string StudentId { get; set; }
+        public string CourseId { get; set; }
+        public bool Eligability { get; set; }
+
+        public StudentEligability(string a, string b, bool c)
+        {
+            StudentId = a;
+            CourseId = b;
+            Eligability = c;
+        }
+    }
+}

352463ba97b3b21391fc93efb551f1366e5ebbe9
mikah13 <anhminhhoang13@gmail.com>
2019-05-16 14:28:25 -0700

    henlo

--- ClientApp/src/app/services/authentication.service.ts
+++ ClientApp/src/app/services/authentication.service.ts
@@ -21,7 +21,7 @@ export class AuthenticationService {
       .pipe(map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          //  user details and jwt token in local storage to keep user logged in between page refreshes
           // localStorage.setItem('currentUser', JSON.stringify(user));
           return user;
         }
